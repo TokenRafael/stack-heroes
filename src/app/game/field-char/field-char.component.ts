8288cfd5e33da81a49716f5cd11989ddabb19960
Rafael <rafaeloliveira@tokenlab.com.br>
2020-12-23 15:41:53 -0300

    feat(game): field-char now hasw damage animation

--- src/app/game/field-char/field-char.component.ts
+++ src/app/game/field-char/field-char.component.ts
@@ -1,3 +1,4 @@
+import { animate, keyframes, style, transition, trigger } from '@angular/animations';
 import { Component, Input, OnInit, Output } from '@angular/core';
 import { EventEmitter } from 'events';
 
@@ -7,7 +8,33 @@ import { MessageService } from '../message.service';
 @Component({
   selector: 'app-field-char',
   templateUrl: './field-char.component.html',
-  styleUrls: ['./field-char.component.scss']
+  styleUrls: ['./field-char.component.scss'],
+  animations: [
+    trigger('onDamageAnimation', [
+      transition('* => dam',
+        animate('2s', keyframes([
+          style({transform: 'translateX(-10%)', offset: 0.15}),
+          style({transform: 'translateX(10%)', offset: 0.30}),
+          style({transform: 'translateX(-10%)', offset: 0.45}),
+          style({transform: 'translateX(10%)', offset: 0.60}),
+          style({transform: 'translateX(-10%)', offset: 0.75}),
+          style({transform: 'translateX(10%)', offset: 0.90}),
+          style({transform: 'translateX(0)', offset: 1}),
+        ]))
+      ),
+      transition('* => damInv',
+        animate('2s', keyframes([
+          style({transform: 'rotateY(180deg) translateX(-10%)', offset: 0.15}),
+          style({transform: 'rotateY(180deg) translateX(10%)', offset: 0.30}),
+          style({transform: 'rotateY(180deg) translateX(-10%)', offset: 0.45}),
+          style({transform: 'rotateY(180deg) translateX(10%)', offset: 0.60}),
+          style({transform: 'rotateY(180deg) translateX(-10%)', offset: 0.75}),
+          style({transform: 'rotateY(180deg) translateX(10%)', offset: 0.90}),
+          style({transform: 'rotateY(180deg) translateX(0)', offset: 1}),
+        ]))
+      )
+    ])
+  ]
 })
 export class FieldCharComponent implements OnInit {
 
@@ -16,6 +43,8 @@ export class FieldCharComponent implements OnInit {
   @Input() chosen = false;
 
   @Output() Click = new EventEmitter();
+
+  animState = null;
 
   constructor(
     private msgService: MessageService
@@ -29,7 +58,12 @@ export class FieldCharComponent implements OnInit {
     this.hero.damage(10);
     this.msgService.setMessage(`${this.hero.name} says ${this.hero.moveset[0].message}`);
 
+    this.animState = this.flipped ? 'damInv' : 'dam';
     this.Click.emit('');
+  }
+
+  resetAnim(): void {
+    this.animState = null;
   }
 
   checkHealth(): boolean {
